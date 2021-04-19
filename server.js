3db8fc22a5eeaad8c34da27f8eb318373209c8b4
denmanj <denman.j@northeastern.edu>
2021-04-19 15:49:22 -0400

    changed app.listen

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ require('./controllers/quizzes-controller')(app)
 require('./controllers/questions-controller')(app)
 require('./controllers/quiz-attempts-controller')(app)
 
-app.listen(4000)
+//app.listen(4000)
+app.listen()
